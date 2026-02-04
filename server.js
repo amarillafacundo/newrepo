d2cad5e1f63dd6b73be098a487c40d6147092bf5
amarillafacundo <facundo.e.amarilla@gmail.com>
2026-02-03 23:56:19 -0300

    update Sessions and Messages

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const session = require("express-session")
+const pool = require('./database/')
 
 /* ***********************
  * View Engine and Template
@@ -20,6 +22,28 @@ const static = require("./routes/static")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
 
 
 /* ***********************
